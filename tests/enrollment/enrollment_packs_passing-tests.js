5fe1901c243441e8699fcc96c2b99438cd8fe32b
ArtemBasiev-Bizapps <basiev.a@webconsultants.ru>
2019-09-11 16:56:53 +0300

    add test case for enrollment packs tests with choosen enroller

--- tests/enrollment/enrollment_packs_passing-tests.js
+++ tests/enrollment/enrollment_packs_passing-tests.js
@@ -1,4 +1,3 @@
-const uuidv1 = require('uuid/v1');
 const { getConfig } = require('../../config');
 const enrollment = require('../../page-objects/enrollment');
 
@@ -30,10 +29,21 @@ module.exports = {
      .assert.urlContains('packs');
   },
 
-  "Enrollment packs should pass with added pack": function(browser) {
+  "Enrollment packs should pass with added pack and with enroller": function(browser) {
     browser
      .waitForElementPresent(enrollment.btnContinue, getConfig().timeout)
      .click(enrollment.btnContinue)
+     .waitForElementPresent(enrollment.btnChooseEnroller, getConfig().timeout)
+     .click(enrollment.btnChooseEnroller)
+     .pause(getConfig().timeout)
+     .waitForElementPresent(enrollment.enrollerSearchField, getConfig().timeout)
+     .clearValue(enrollment.enrollerSearchField)
+     .setValue(enrollment.enrollerSearchField, getConfig().testEnrollerName)
+     .waitForElementPresent(enrollment.btnSearchEnroller, getConfig().timeout)
+     .click(enrollment.btnSearchEnroller)
+     .waitForElementPresent(enrollment.btnUseDestributor, getConfig().timeout)
+     .click(enrollment.btnUseDestributor)
+     .pause(getConfig().timeout)
      .waitForElementPresent(enrollment.btnJoin, getConfig().timeout)
      .click(enrollment.btnJoin)
      .waitForElementPresent(enrollment.btnAddBasicKit, getConfig().timeout)
@@ -43,7 +53,5 @@ module.exports = {
      .pause(getConfig().timeout)
      .assert.urlContains('enrolleeinfo');
   }
-
-
 
 };
